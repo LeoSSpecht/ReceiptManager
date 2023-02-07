8c4ba921a4cb9f742182337ad9b105119a6d8ef9
LeoSSpecht <62398409+LeoSSpecht@users.noreply.github.com>
2023-02-07 18:21:10 -0500

    Changed API Keys

--- Frontend/ReceiptManager/src/firebase/config.js
+++ Frontend/ReceiptManager/src/firebase/config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDzNGebQ9KH4SOcS-XeD44qbpnZAIC4o6s",
-    authDomain: "receiptmanager10.firebaseapp.com",
-    projectId: "receiptmanager10",
-    storageBucket: "receiptmanager10.appspot.com",
-    messagingSenderId: "803781307265",
-    appId: "1:803781307265:web:ae10a76d238ec048ced680",
-    measurementId: "G-S4T2QX2G5E"
-  };
+  apiKey: "AIzaSyDzNGebQ9KH4SOcS-XeD44qbpnZAIC4o6s",
+  authDomain: "receiptmanager10.firebaseapp.com",
+  projectId: "receiptmanager10",
+  storageBucket: "receiptmanager10.appspot.com",
+  messagingSenderId: "803781307265",
+  appId: "1:803781307265:web:3dd7f7efb763ff60ced680",
+  measurementId: "G-Q1JF3LYTN8"
+};
 
 const app = initializeApp(firebaseConfig);
 export { app };
